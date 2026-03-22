9e0099246a9dace66c4e64ad340e63f9606306f8
lakhouil maryem <lkhwilmaryem@gmail.com>
2026-03-22 19:18:12 +0100

    ajouter le model cours

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Course extends Model
 {
-    //
+    protected $fillable = [
+        'titre',
+        'description',
+        'prix',
+        'enseignant_id',
+    ];
+
+    public function enseignant()
+    {
+        return $this->belongsTo(User::class, 'enseignant_id');
+    }
 }
