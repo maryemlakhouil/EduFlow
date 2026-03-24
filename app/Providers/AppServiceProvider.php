ba108255bd4c6f7d402996f6f079c73d3f9553f5
lakhouil maryem <lkhwilmaryem@gmail.com>
2026-03-24 09:07:47 +0100

    enregistrer favoretclass dans appserviceprovider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,8 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use App\Repositories\Contracts\CourseRepositoryInterface;
 use App\Repositories\Eloquent\CourseRepository;
+use App\Repositories\Contracts\FavoretlistRepositoryInterface;
+use App\Repositories\Eloquent\FavoretlistRepository;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,7 +16,9 @@ class AppServiceProvider extends ServiceProvider
     public function register(): void
     {
          $this->app->bind(CourseRepositoryInterface::class, CourseRepository::class);
+         $this->app->bind(WishlistRepositoryInterface::class,WishlistRepository::class);
     }
+
 
     /**
      * Bootstrap any application services.
