6d707ae6094ced8ecde10b1af9d253c3290ee8c9
lakhouil maryem <lkhwilmaryem@gmail.com>
2026-03-27 16:46:45 +0100

    afficher la page pour cancel la painement

---
+++ resources/views/payment/success.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Paiement réussi</title>
+</head>
+<body>
+
+<h1>Paiement réussi !</h1>
+<p>Vous êtes maintenant inscrit au cours.</p>
+
+<a href="/courses">Voir mes cours</a>
+
+</body>
+</html>
